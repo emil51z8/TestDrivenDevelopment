715f6bc00510906a2c4f92d284fd54127df92fb0
emil51z8 <emil51z8@edu.zealand.dk>
2023-10-25 11:57:16 +0200

    opgaven lavet og test'ne er skrevet

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
+
+    public int occurence (String input, char character) {
+        int result = 0;
+        if (input != null) {
+            for (int i = 0; i < input.length(); i++) {
+                if (Character.toLowerCase(input.charAt(i)) == Character.toLowerCase(character)) {
+                    result++;
+                }
+            }
+            return result;
+        }
+        return -1;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
--- src/MainTest.java
+++ src/MainTest.java
@@ -1,4 +1,52 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+
 class MainTest {
-  
+    Main main = new Main();
+
+    @org.junit.jupiter.api.Test
+    void occurenceOfSingleChar(){
+        String stringToCheck = "hej";
+        char charTofind = 'e';
+        int expectedResult = 1;
+        int actualResult = main.occurence(stringToCheck,charTofind);
+        Assertions.assertEquals(expectedResult,actualResult);
+    }
+
+    @org.junit.jupiter.api.Test
+    void NotSingleOccurenceOfChar() {
+        String stringToCheck = "eee";
+        char charTofind = 'e';
+        int expectedResult = 1;
+        int actualResult = main.occurence(stringToCheck,charTofind);
+        Assertions.assertNotEquals(expectedResult,actualResult);
+    }
+
+    @org.junit.jupiter.api.Test
+    void MultipleInstancesOfOccurence () {
+        String stringToCheck = "heeeeej";
+        char charTofind = 'e';
+        int expectedResult = 5;
+        int actualResult = main.occurence(stringToCheck,charTofind);
+        Assertions.assertEquals(expectedResult,actualResult);
+    }
+
+    @org.junit.jupiter.api.Test
+    void intensitiveOccurenceOfChar(){
+        String stringToCheck = "heEeeEj";
+        char charTofind = 'e';
+        int expectedResult = 5;
+        int actualResult = main.occurence(stringToCheck,charTofind);
+        Assertions.assertEquals(expectedResult,actualResult);
+    }
+
+
+    @org.junit.jupiter.api.Test
+    void nullTest() {
+        String stringToCheck = null;
+        char charTofind = 'e';
+        int expectedResult = -1;
+        int actualResult = main.occurence(stringToCheck,charTofind);
+        Assertions.assertEquals(expectedResult,actualResult);
+    }
+
 }
